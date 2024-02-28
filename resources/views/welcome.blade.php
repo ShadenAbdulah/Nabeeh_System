b92c41bf9878ea72d86e827ad02a86d04819eed7
ShadenAbdulah <sh22m2015@gmail.com>
2024-02-28 17:01:56 +0300

    Ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
 <x-head session_name="none">
     <body>
-    <img src="{{asset('images/welcome_BG.png')}}" alt="bg">
+    <img src="{{asset('images/welcome_BG.png')}}" alt="bg" class="mx-auto pr-10">
     <div class="container w-full mx-auto my-auto">
-        <img src="{{asset('images/Logo.svg')}}">
+{{--        <img src="{{asset('images/Logo.svg')}}" class="p-20">--}}
+        <x-application-logo class="p-10"></x-application-logo>
 
     </div>
     </body>
