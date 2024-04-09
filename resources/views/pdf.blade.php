d541f366ec9dbd1cca69a1c8b52c5a35bbbd1bcd
ShadenAbdulah <sh22m2015@gmail.com>
2024-04-09 06:22:02 +0300

    pdf

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,4 +1,11 @@
-@extends('layouts.app')
-@section('content')
-    <div>السلام</div>
-@endsection
+<x-head session_name="none" sample_id="none" test_id="none">
+
+    <htmlpageheader name="page-header">
+        Your Header Content
+    </htmlpageheader>
+
+    <htmlpagefooter name="page-footer">
+        Your Footer Content
+    </htmlpagefooter>
+
+</x-head>
