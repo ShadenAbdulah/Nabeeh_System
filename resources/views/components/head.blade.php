85a4d047df2004cfe70fb09a934bbf6e72866ac2
EAnhar <91504512+EAnhar@users.noreply.github.com>
2024-04-13 11:40:19 +0300

    zoom

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -3,7 +3,7 @@
 <html lang="ar" dir="rtl">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- PWA  -->
     <meta name="theme-color" content="#6777ef"/>
@@ -17,7 +17,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     {{--    <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap"--}}
     {{--    rel="stylesheet">--}}
-    <link rel="stylesheet" href="{{asset('build/assets/app-37e86996.css')}}">
+    <link rel="stylesheet" href="{{asset('build/assets/app-8d1bbe8f.css')}}">
 
 
     {{--    @vite(['resources/js/app.js', 'resources/css/app.css'])--}}
