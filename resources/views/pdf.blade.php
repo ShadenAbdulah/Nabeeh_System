a4ec9e453365459996e1852c49b8058f795a22ae
ShadenAbdulah <sh22m2015@gmail.com>
2024-04-13 16:14:27 +0300

    pdf

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,3 +1,7 @@
-@extends('layouts.app')
-@section('content')
-@endsection
+<x-head>
+    <body class="font-[Tajawal] flex flex-col min-h-screen">
+    <page size="A4">
+        <div>page</div>
+    </page>
+    </body>
+</x-head>
