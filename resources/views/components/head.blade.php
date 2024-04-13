d0eff25348fc6c174f6b134cfb21c920a700e945
EAnhar <91504512+EAnhar@users.noreply.github.com>
2024-04-13 11:22:52 +0300

    non zoom

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -3,7 +3,7 @@
 <html lang="ar" dir="rtl">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- PWA  -->
     <meta name="theme-color" content="#6777ef"/>
