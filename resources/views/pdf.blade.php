409a819140c617c264f174bf4701626c1ad79b2f
ShadenAbdulah <sh22m2015@gmail.com>
2024-04-13 16:16:23 +0300

    pdf

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,7 +1,7 @@
 <x-head session_name="none" sample_id="none" test_id="none">
     <body class="font-[Tajawal] flex flex-col min-h-screen">
-    <page size="A4">
-        <div>page</div>
-    </page>
+    {{--    <page size="A4">--}}
+    {{--        <div>page</div>--}}
+    {{--    </page>--}}
     </body>
 </x-head>
