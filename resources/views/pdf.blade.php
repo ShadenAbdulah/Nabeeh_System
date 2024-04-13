fd88504eaf8593188dbeda4ce084961064f1965b
ShadenAbdulah <sh22m2015@gmail.com>
2024-04-13 16:48:30 +0300

    pdf

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -21,9 +21,9 @@
         <div class="text-center">جميـع الحقوق محفوظــة © 2024</div>
         <div class="flex flex-row-reverse gap-8">
 
-            <img src="{{asset('images/mail_icon.png')}}" size="10">
+            <img src="{{asset('images/mail_icon.png')}}" size="10x10">
             <img src="{{asset('images/whatsapp_icon.png')}}" size="100">
-            <img src="{{asset('images/X_icon.png')}}">
+            <img src="{{asset('images/X_icon.png')}}" class="w-1 h-1">
 
         </div>
     </div>
