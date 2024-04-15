c1f3321a5c9850eb7fa7f30df82b2fe5362b235a
ShadenAbdulah <sh22m2015@gmail.com>
2024-04-15 17:44:10 +0300

    pdf

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -14,8 +14,8 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="container mx-auto font-[Tajawal]">
-<div class="w-full content-center border">
-    <img src="{{asset('images/Logo.svg')}}" class="w-10 mx-auto" alt="logo"/>
+<div class="w-full content-center justify-center mx-auto">
+    <img src="{{asset('images/Logo.svg')}}" class="w-32 mx-auto" alt="logo"/>
 </div>
 
 
