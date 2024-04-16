5c4c77012dab9c65cc1877bd1e878839f1618477
ShadenAbdulah <sh22m2015@gmail.com>
2024-04-16 13:22:12 +0300

    translate

--- resources/views/layouts/test.blade.php
+++ resources/views/layouts/test.blade.php
@@ -22,8 +22,8 @@
             @endfor
         </div>
         <h1 class="font-semibold"><span
-                class="text-[#6E65E8]">{{__('extra.'.$test->type)}} </span>{{__($test->title)}}</h1>
-        <p class="font-medium text-justify">{{$test->content}} سيظهر لك خط متعرج والمطلوب منك هو إعادة رسم
+                class="text-[#6E65E8]">{{__('extra.'.$test->type)}} </span>{{__('extra.'.$test->title)}}</h1>
+        <p class="font-medium text-justify">{{'extra.'.$test->content}} سيظهر لك خط متعرج والمطلوب منك هو إعادة رسم
             الخط دون رفع القلم عن الجهاز اللوحي وتكرار ذلك على السطور الخمسة التي ستظهر لك على الشاشة، خلال 30
             ثانية.</p>
     </div>
