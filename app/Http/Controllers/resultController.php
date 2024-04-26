5e3750cf4df6e96d35f957790a5be4720306da45
EAnhar <91504512+EAnhar@users.noreply.github.com>
2024-04-26 12:50:45 +0300

    error

--- app/Http/Controllers/resultController.php
+++ app/Http/Controllers/resultController.php
@@ -41,13 +41,15 @@ class resultController extends Controller
 
                 return response()->json(['value' => $value, 'prop' => $prop]);
             } else {
+                $errorResponse = json_decode($response->body(), true);
                 Log::error('Error from API:', ['response' => $response->body()]);
-                return response()->json(['error' => $response->body() ?? 'Failed to get a valid response from the API.'], 500);
+                return response()->json(['error' => $errorResponse ?? 'Failed to get a valid response from the API.'], 500);
             }
-
+    
         } catch (Exception $e) {
             Log::error('Lambda execution failed: ' . $e->getMessage());
             return view('server500');
         }
     }
 }
+
