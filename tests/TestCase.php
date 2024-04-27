0d92765aee7fde6383d882ba22ee8baa0b585b1f
ShadenAbdulah <sh22m2015@gmail.com>
2024-04-27 21:08:12 +0300

    test user flow

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function refreshDatabase()
+    {
+        // Override and make this method a no-op
+    }
 }
