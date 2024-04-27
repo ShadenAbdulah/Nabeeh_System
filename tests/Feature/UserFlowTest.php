7c6209d6c7ace79b7b902eb34ef0ce738f45e9e2
ShadenAbdulah <sh22m2015@gmail.com>
2024-04-27 18:01:53 +0300

    test user flow

--- tests/Feature/UserFlowTest.php
+++ tests/Feature/UserFlowTest.php
@@ -21,7 +21,7 @@ class UserFlowTest extends TestCase
         $response->assertStatus(200);
 
         // Simulate the user starting the exam
-        $response = $this->post('/store/5', ['user' => 5]); // Adjust the route and data as per your application
+        $response = $this->post('/store', ['user' => 5]); // Adjust the route and data as per your application
         $response->assertStatus(200); // Assuming successful start of the exam
 
         // Simulate the user accessing the result view by exam ID
