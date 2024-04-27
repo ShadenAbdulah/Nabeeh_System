78b89f02e004ee0c2dc7c6c7a0f52141d9f40f6d
ShadenAbdulah <sh22m2015@gmail.com>
2024-04-27 18:00:31 +0300

    test user flow

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -13,7 +12,6 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Test extends Model
 {
-    use HasFactory;
 
     protected $table = 'tests';
 
